8d7442a66f28b6ae4e2c23b1391e75bcc4967e24
Cédric Bellavance <cedricbellavance337@gmail.com>
2026-08-17 12:32:00 -0400

    Add centralized error handling middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import healthRoutes from "./routes/healthRoutes.js";
 import customerRoutes from "./routes/customerRoutes.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(express.json());
 
 app.use("/api/health", healthRoutes);
 app.use("/api/customers", customerRoutes);
+
+app.use(errorHandler);
 
 export default app;
